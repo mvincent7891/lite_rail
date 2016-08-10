9996e308ad1c7e4b1ac7c8c4602e63a50c79d389
Michael Parlato <michaelvparlato@gmail.com>
2016-08-09 17:33:56 -0700

    Started exception handling

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -1,15 +1,30 @@
 require 'erb'
+require 'byebug'
 
 class ShowExceptions
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    begin
+      @app.call(env)
+    rescue => e
+      render_exception(e)
+    end
   end
 
   private
 
   def render_exception(e)
+    # ['200', {'Content-Type' => 'text/html'}, ['hello world']]
+    status_code = 100.to_s
+    content_type = {'Content-Type' => 'text/html'}
+    content = ['there is an error']
+
+    [status_code, content_type, content]
   end
 
 end
