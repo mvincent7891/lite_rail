c60055658ab2bd4adb80fbe104236d6cc47c39e2
Michael Parlato <michaelvparlato@gmail.com>
2016-08-10 12:25:02 -0700

    Added error handling with stack trace, source code preview, and error message

--- lib/show_exceptions.rb
+++ lib/show_exceptions.rb
@@ -12,16 +12,37 @@ class ShowExceptions
     begin
       @app.call(env)
     rescue => e
-      render_exception(e)
+      render_exception(e, env)
     end
   end
 
   private
 
-  def render_exception(e)
-    message = "hello world"
-    # will show message when error is raised in app
-    ['200', {'Content-Type' => 'text/html'}, [message]]
+  def render_exception(e, env = nil)
+
+    template_path = "lib/templates/rescue.html.erb"
+    # stack_regex = Regexp.new("stack_depth=(\d+)")
+    @path = env["PATH"]
+    if env && env["QUERY_STRING"].match(Regexp.new("stack_depth"))
+      @stack_depth = env["QUERY_STRING"].match(Regexp.new("\\d+"))[0].to_i
+    else
+      @stack_depth = 3
+    end
+    @error_message = e.message
+    @backtrace = e.backtrace
+    file_regex = Regexp.new("(.+):(\\d+)")
+    match_data = e.backtrace[0].match(file_regex)
+    @file_name = match_data[1]
+    @line = match_data[2].to_i
+    start_line = @line - 4
+    end_line = @line + 2
+    @source = []
+    File.readlines(@file_name)[start_line..end_line].each_with_index do |line, index|
+      @source << "Line #{index + @line - 3}: #{line}"
+    end
+
+    evaluated_erb_template = ERB.new(File.read(template_path)).result(binding)
+    ['200', {'Content-Type' => 'text/html'}, [evaluated_erb_template]]
   end
 
 end
--- lib/static.rb
+++ lib/static.rb
@@ -1,7 +1,12 @@
 class Static
+  attr_reader :app
+
   def initialize(app)
+    @app = app
   end
 
   def call(env)
+    @app.call(env)
   end
+  
 end
